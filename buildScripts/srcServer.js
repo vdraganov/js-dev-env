1bdc6369f0e5c1d6130cce9bbf3bb02752797cd0
Valentin Draganov <vdraganov@hotmail.co.uk>
2017-07-23 15:12:39 +0300

    adding webpack bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.dev';
 
 const port = 3000;
 
 const app = express();
+const compiler = webpack(webpackConfig);
+
+// configuring Express to use webpack
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: webpackConfig.output.publicPath
+}));
 
 app.get('', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
